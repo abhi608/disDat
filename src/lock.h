c2d6d6cb2f7add7329e96003db7f77009f236347
Abhishek Verma <608abhishek@gmail.com>
2022-12-01 17:38:54 +0000

    bug fixes

--- src/lock.h
+++ src/lock.h
@@ -8,14 +8,14 @@
 class Lock {
 private:
     LockType lock_type;
-    Transaction transaction;
+    Transaction* transaction;
 
 public:
-    Lock(const LockType lock_type_, const Transaction& transaction_);
-    Transaction& get_transaction();
+    Lock(const LockType lock_type_, Transaction* transaction_);
+    Transaction* get_transaction();
     LockType get_lock_type() const;
     void set_lock_type(const LockType lock_type_);
-    void set_transaction(const Transaction& transaction_);
+    void set_transaction(Transaction* transaction_);
     bool operator==(const Lock& lock_) const;
 };
 
